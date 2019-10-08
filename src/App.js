7af5906f60f2702ff4b783421f591b4b879b2ac0
theJOHNwatt <j.watt10@gmail.com>
2019-10-07 23:10:09 -0700

    productscontext finished and is refactored and working properly. add cartcontext

--- src/App.js
+++ src/App.js
@@ -23,23 +23,9 @@ function App() {
 		<div className="App">
 			<ProductContext.Provider value={{products, addItem}} >
 			<Navigation cart={cart} />
-
 			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
-
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+			<Route exact path="/" component={Products} />	
+			<Route path="/cart" component={ShoppingCart} />
 			</ProductContext.Provider>
 		</div>
 	);
