0e9cdb2944bb701ad2757fa2699d6ea5779ea43c
theJOHNwatt <j.watt10@gmail.com>
2019-10-07 23:01:51 -0700

    initial commit. added context folder and productscontext. added productscontext to app and starting to refactor

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+import {ProductContext} from './context/ProductContext';
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import Product from './components/Product';
 
 function App() {
 	const [products] = useState(data);
@@ -13,10 +16,12 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart, item]);
 	};
 
 	return (
 		<div className="App">
+			<ProductContext.Provider value={{products, addItem}} >
 			<Navigation cart={cart} />
 
 			{/* Routes */}
@@ -35,6 +40,7 @@ function App() {
 				path="/cart"
 				render={() => <ShoppingCart cart={cart} />}
 			/>
+			</ProductContext.Provider>
 		</div>
 	);
 }
